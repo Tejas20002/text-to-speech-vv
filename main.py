0bff9b6456b23b6fc031e07983d5627f5ed162a0
Tejas20002 <tejashirani55@gmail.com>
2023-03-10 20:44:57 +0530

    [*] text to speech done

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import os
 from googletrans import Translator
 import googletrans
 import speech_recognition as sr
+import pyttsx3
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QInputDialog, QMessageBox
 from design import Ui_MainWindow
 
 class Main(QtWidgets.QMainWindow):
+    outText=None
     def __init__(self,parent=None):
         super(Main, self).__init__(parent=parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.addLanguange()
         self.ui.translat_button.clicked.connect(self.translate)
+        self.ui.texttospeech.clicked.connect(self.textToSpeech)
 
     def addLanguange(self):
         self.ui.outLan.addItems(googletrans.LANGUAGES.values())
@@ -27,7 +30,8 @@ class Main(QtWidgets.QMainWindow):
             translator = Translator()
             translate = translator.translate(inputText, dest=f'{lang_out}')
             print(translate)
-            self.ui.output_text.setText(translate.text)
+            self.ui.output_text.setText(f"{translate.text}\n{translate.pronunciation}")
+            self.outText = translate.pronunciation
         except Exception as e:
             self.errorMessage(e)
 
@@ -38,6 +42,11 @@ class Main(QtWidgets.QMainWindow):
         msg.setWindowTitle('Error!!')
         msg.setText(str(message))
         msg.exec_()
+
+    def SpeakText(self, command):
+        engine = pyttsx3.init()
+        engine.say(command)
+        engine.runAndWait()
 
     def speechToText(self):
         r = sr.Recognizer()
@@ -55,6 +64,13 @@ class Main(QtWidgets.QMainWindow):
             # write audio
             with open("recorded.wav", "wb") as f:
                 f.write(audio.get_wav_data())
+
+    def textToSpeech(self):
+        inText = self.ui.input_text.toPlainText()
+        outText = self.ui.output_text.toPlainText()
+        self.SpeakText(inText)
+        self.SpeakText(outText)
+        self.SpeakText(self.outText)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
